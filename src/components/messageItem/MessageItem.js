565fc6042efcd153fd4da6e82e8fff59d6d0b97e
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-09-02 14:24:35 -0400

    Implemented Message Feed, worked with entire group

--- src/components/messageItem/MessageItem.js
+++ src/components/messageItem/MessageItem.js
@@ -6,6 +6,11 @@ function MessageItem ({user, text, id, date, likes}){
         <div className="MessageItem" style={{border: '1px solid black'}}>
             <h3>{user}</h3>
             <p>{text}</p>
+            <ul>
+                <li>Users ID: {id}</li>
+                <li>Date Created: {date}</li>
+                <li>Likes: {likes.length}</li>
+            </ul>
         </div>
     )
     }
