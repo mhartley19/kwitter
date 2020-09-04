a90b5fb5637c6f1eacc550777aa498783b43930c
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-09-04 14:52:48 -0400

    Cleaned up code, removed comments

--- src/components/messageItem/MessageItem.js
+++ src/components/messageItem/MessageItem.js
@@ -1,35 +1,45 @@
-import React from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-import {toggleLike} from '../../redux/actions/likeAction'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleLike } from "../../redux/actions/likeAction";
 
-function MessageItem ({user, text, id, date, likes}){
-    const dispatch = useDispatch()
-    const username = useSelector(state => state.auth.username)
-    const isLiked = () => {
-        // console.log(`username: ${username} id: ${id} liked: ${likes.map(like => like.username ===  username)
-        return (likes.some(like => like.username === username ))
-    }
-  
-    const getLikeId = () => {
-        if(isLiked()){
-            // return likes.filter((like) => like.username === username ? like.id : null )
-            const user = likes.filter((like) => like.username === username)
-            return user[0].id
-        }
-    }
+function MessageItem({ user, text, id, date, likes }) {
+  const dispatch = useDispatch();
+  const username = useSelector((state) => state.auth.username);
+  const isLiked = () => {
+    return likes.some((like) => like.username === username);
+  };
 
-    
-    return (
-        <div className="MessageItem" style={{border: '1px solid black'}}>
-            <h3>{user}</h3>
-            <p>{text}</p>
-            <ul>
-                <li>Users ID: {id}</li>
-                <li>Date Created: {date}</li>
-                <li>Likes: {likes.length}</li>
-                <input defaultChecked={isLiked()} type="checkbox" onChange={() => dispatch(toggleLike(isLiked(), id, getLikeId()))}/>
-            </ul>
-        </div>
-    )
+  const getLikeId = () => {
+    if (isLiked()) {
+      const user = likes.filter((like) => like.username === username);
+      return user[0].id;
     }
-export default MessageItem
+  };
+
+  return (
+    <div
+      className="MessageItem"
+      style={{
+        border: "1px solid black",
+        backgroundColor: "rgb(220, 223, 232 )",
+        padding: "10px",
+        margin: "15px",
+        width: "400px",
+      }}
+    >
+      <h3>{user}</h3>
+      <p>{text}</p>
+      <ul>
+        <li>Users ID: {id}</li>
+        <li>Date Created: {date}</li>
+        <li>Likes: {likes.length}</li>
+        <input
+          defaultChecked={isLiked()}
+          type="checkbox"
+          onChange={() => dispatch(toggleLike(isLiked(), id, getLikeId()))}
+        />
+      </ul>
+    </div>
+  );
+}
+export default MessageItem;
