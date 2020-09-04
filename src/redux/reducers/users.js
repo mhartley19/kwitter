fb90ef9e1274ec04a97da0bc50dd563015c05bf0
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-09-04 14:47:14 -0400

    Created user reducer for Profile Page

--- src/redux/reducers/users.js
+++ src/redux/reducers/users.js
@@ -1,1 +1,42 @@
-// TODO: implement
+import { GET_USER_INFO, PUT_PICTURE } from "../actions/userProfile";
+
+const initialState = {
+  pictureLocation: "",
+  username: "",
+  displayName: "",
+  about: "",
+  googleId: "",
+  createdAt: "",
+  updatedAt: "",
+};
+
+const getUserReducer = (state = { ...initialState }, action) => {
+  switch (action.type) {
+    case GET_USER_INFO:
+      const {
+        pictureLocation,
+        username,
+        displayName,
+        about,
+        googleId,
+        createdAt,
+        updatedAt,
+      } = action.payload.user;
+      return {
+        ...state,
+        pictureLocation,
+        username,
+        displayName,
+        about,
+        googleId,
+        createdAt,
+        updatedAt,
+      };
+    case PUT_PICTURE:
+      return state;
+    default:
+      return state;
+  }
+};
+
+export default getUserReducer;
