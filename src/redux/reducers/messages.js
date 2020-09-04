8c329c2f68623acde56721045d8de33e4c96ea04
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-09-04 09:45:19 -0400

    Cleaned up code, removed unnecassary spaces/gaps

--- src/redux/reducers/messages.js
+++ src/redux/reducers/messages.js
@@ -25,15 +25,10 @@ const messageReducer = (state = initialState, action) =>{
             return{
                 ...state, loading: false, error: "Something went wrong. WTF!"
             }
-
-
-
         case POST_NEW_MESSAGE:
             return{
                 ...state, newMessageValue: [action.payload.message]
             }
-
-
         case UPDATE_MESSAGE:
             return {...state, messages: [...state.messages.map((message) => message.id === action.payload.id ? action.payload : message )] }
 
