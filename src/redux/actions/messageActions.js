0ec1b8fdf0c7e1b66683b3c3a567a79e0fcfbc99
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-09-08 09:37:16 -0400

    Created getUser actions & reducer

--- src/redux/actions/messageActions.js
+++ src/redux/actions/messageActions.js
@@ -1,35 +1,44 @@
 import api from "../../utils/api";
 
-export const FETCH_MESSAGES = 'FETCH MESSAGES'
-export const LOADING_MESSAGES = 'LOADING MESSAGES'
-export const INITIATE_SUCCESS = 'INITIATE SUCCESS'
-export const INITIATE_FAILURE = 'INITIATE FAILURE'
-export const NEW_MESSAGE = 'NEW MESSAGE'
-export const POST_NEW_MESSAGE = 'POST NEW MESSAGE'
-
+export const FETCH_MESSAGES = "FETCH MESSAGES";
+export const LOADING_MESSAGES = "LOADING MESSAGES";
+export const INITIATE_SUCCESS = "INITIATE SUCCESS";
+export const INITIATE_FAILURE = "INITIATE FAILURE";
+export const NEW_MESSAGE = "NEW MESSAGE";
+export const POST_NEW_MESSAGE = "POST NEW MESSAGE";
+export const GOT_USER_MESSAGES = "GOT USER MESSAGES";
 
 export const fetchMessages = () => async (dispatch, getState) => {
-    try {
-        dispatch({ type: LOADING_MESSAGES });
-        const payload = await api.initiateMessages();
-        dispatch({type: INITIATE_SUCCESS, payload})
-    }
-    catch (err){
-        console.log(err.message)
-        dispatch({type: INITIATE_FAILURE, payload: "Something went wrong! OH MY GOD!"})
-    }
-    
-}
+  try {
+    dispatch({ type: LOADING_MESSAGES });
+    const payload = await api.initiateMessages();
+    dispatch({ type: INITIATE_SUCCESS, payload });
+  } catch (err) {
+    console.log(err.message);
+    dispatch({
+      type: INITIATE_FAILURE,
+      payload: "Something went wrong! OH MY GOD!",
+    });
+  }
+};
 
 export const newMessage = (data) => async (dispatch) => {
-    try{
-    
-        const payload = await api.createNewMessage(data)
-        dispatch({type: POST_NEW_MESSAGE, payload})
-        .then(dispatch({type: INITIATE_SUCCESS, payload}))
+  try {
+    const payload = await api.createNewMessage(data);
+    dispatch({ type: POST_NEW_MESSAGE, payload }).then(
+      dispatch({ type: INITIATE_SUCCESS, payload })
+    );
+  } catch (err) {}
+};
 
-    }
-     catch(err){
-    
-}
-}
+export const userMessages = (user) => async (dispatch) => {
+  try {
+    const payload = await api.getUserMessages(user);
+    dispatch({ type: GOT_USER_MESSAGES, payload });
+  } catch (err) {
+    dispatch({
+      type: INITIATE_FAILURE,
+      payload: "Something went wrong! OH MY GOD!",
+    });
+  }
+};
