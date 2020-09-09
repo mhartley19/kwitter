668a22d9fef4b5630f362bf9cc736fa919d377d1
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-09-09 13:30:29 -0400

    Adding styling with component library

--- src/components/messageItem/MessageItem.js
+++ src/components/messageItem/MessageItem.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Card, ToggleButton, ButtonGroup } from "react-bootstrap";
+import React from "react";
+import { Card, ToggleButton, ButtonGroup, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleLike } from "../../redux/actions/likeAction";
@@ -8,9 +8,9 @@ import defaultPhoto from "../default_photo.jpg";
 
 function MessageItem({ user, text, id, date, likes }) {
   const dispatch = useDispatch();
-  const [checked, setChecked] = useState(false);
 
   const username = useSelector((state) => state.auth.username);
+
   const isLiked = () => {
     return likes.some((like) => like.username === username);
   };
@@ -26,16 +26,17 @@ function MessageItem({ user, text, id, date, likes }) {
   };
   const DeleteButton = () => {
     return (
-      <button
+      <Button
+        variant="primary"
         id={id}
         className="deleteMessageButton"
         onClick={() => handleDelete()}
       >
         Delete
-      </button>
+      </Button>
     );
   };
-  console.log();
+
   return (
     <>
       <Card
@@ -49,25 +50,50 @@ function MessageItem({ user, text, id, date, likes }) {
           border: "1px solid black",
           backgroundColor: "rgb(220, 223, 232 )",
           margin: "15px",
-          width: "400px",
+          width: "500px",
         }}
       >
-        <Card.Header style={{ backgroundColor: "rgb(0, 31, 126)" }}>
-          {" "}
-          <Card.Img
-            src={`https://kwitter-api.herokuapp.com/users/${user}/picture`}
-            style={{ width: "50px", borderRadius: "50%" }}
-          />
-          {"    "}
-          <Card.Title> {user}</Card.Title>
+        <Card.Header
+          style={{
+            backgroundColor: "rgb(0, 31, 126)",
+            padding: "5px",
+          }}
+        >
+          <Card.Title
+            style={{
+              margin: "1px",
+            }}
+          >
+            {" "}
+            <Card.Img
+              src={`https://kwitter-api.herokuapp.com/users/${user}/picture`}
+              style={{ width: "50px", borderRadius: "50%" }}
+            />
+            {"    "}
+            {user}
+          </Card.Title>
         </Card.Header>
-        <Card.Body style={{ backgroundColor: "rgb(70, 87, 135 )" }}>
+        <Card.Body
+          style={{
+            backgroundColor: "rgb(70, 87, 135 )",
+            paddingTop: "2px",
+            paddingLeft: "3px",
+          }}
+        >
           <Card.Text>Message:</Card.Text>
-          <blockquote className="blockquote mb-0">
+          <blockquote
+            className="blockquote mb-0"
+            style={{ paddingLeft: "8px" }}
+          >
             <p>{text}</p>
           </blockquote>
         </Card.Body>
-        <Card.Footer style={{ backgroundColor: "rgb(0, 31, 126)" }}>
+        <Card.Footer
+          style={{
+            backgroundColor: "rgb(0, 31, 126)",
+            padding: "5px",
+          }}
+        >
           <ButtonGroup toggle className="mb-2">
             <ToggleButton
               type="checkbox"
@@ -80,7 +106,7 @@ function MessageItem({ user, text, id, date, likes }) {
             </ToggleButton>
           </ButtonGroup>{" "}
           {likes.length} likes <br />
-          Date Created {date}
+          Date Created {date} {username === user ? <DeleteButton /> : null}
         </Card.Footer>
       </Card>
 
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,108 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { MenuContainer } from "../components";
-import MessageItem from "../components/messageItem/MessageItem";
-import { useSelector, useDispatch } from "react-redux";
-import { userMessages } from "../redux/actions/messageActions";
-import { getUserInfo, putUserPicture } from "../redux/actions/userProfile";
+import UserProfile from "../components/userProfile/UserProfile";
 
 export function ProfileScreen() {
-  const picture = {
-    selectedFile: null,
-  };
-
-  const user = useSelector((state) => state.auth);
-  const userInfo = useSelector((state) => state.user);
-  const messages = useSelector((state) => state.messageReducer.userMessages);
-  const dispatch = useDispatch();
-
-  const onFileChange = (event) => {
-    picture.selectedFile = event.target.files[0];
-  };
-
-  const onFileUpload = () => {
-    if (picture.selectedFile) {
-      const pictureData = new FormData();
-      pictureData.append("picture", picture.selectedFile);
-      dispatch(putUserPicture(userInfo.username, pictureData));
-    } else {
-      alert("Please select a file.");
-    }
-  };
-
-  useEffect(() => {
-    dispatch(userMessages(user.username));
-  }, [user]);
-
-  useEffect(() => {
-    dispatch(getUserInfo(user.username));
-  }, []);
-
   return (
     <>
       <MenuContainer />
-      <h2>{userInfo.username}'s Profile Page</h2>
-      <div
-        className="profilePage"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "left",
-        }}
-      >
-        <div
-          className="profileInfo"
-          style={{
-            border: "1px solid black",
-            margin: "15px",
-            padding: "15px",
-            background: "rgb(181, 195, 235)",
-            width: "300px",
-            height: "500px",
-          }}
-        >
-          <img
-            alt={userInfo.username}
-            key={userInfo.username}
-            src={`https://kwitter-api.herokuapp.com${userInfo.pictureLocation}`}
-            style={{ width: "200px" }}
-          />
-          <br />
-          <label>Upload a Picture</label>
-          <br />
-          <input
-            type="file"
-            id="ProfilePicture"
-            name="Add Picture"
-            accept=".gif, .jpeg, .png, .jpg"
-            onChange={onFileChange}
-          />
-          <button onClick={onFileUpload}>Upload!</button>
-          <h4>Username: {userInfo.username}</h4>
-          <p>bio: </p>
-          <p>date joined Kwitter</p>
-        </div>
-        <div
-          className="userMessages"
-          style={{
-            border: "1px solid black",
-            margin: "15px",
-            padding: "15px",
-            background: "rgb(181, 195, 235)",
-            width: "400px",
-          }}
-        >
-          {messages.map((message) => (
-            <MessageItem
-              user={message.username}
-              text={message.text}
-              id={message.id}
-              date={message.createdAt}
-              likes={message.likes}
-              key={message.id}
-            />
-          ))}
-        </div>
-      </div>
+      <UserProfile />
     </>
   );
 }
