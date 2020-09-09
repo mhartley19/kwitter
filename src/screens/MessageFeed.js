426e859a84fb2594a21ee938f241d1ab31f9bcaf
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-09-09 12:26:45 -0400

    Removed uneeded import

--- src/screens/MessageFeed.js
+++ src/screens/MessageFeed.js
@@ -3,7 +3,6 @@ import MessageItem from "../components/messageItem/MessageItem";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMessages, newMessage } from "../redux/actions/messageActions";
 import { MenuContainer } from "../components";
-import api from "../utils/api";
 
 export function MessageFeed() {
   const [newMessageInput, setNewMessageInput] = useState();
@@ -17,7 +16,7 @@ export function MessageFeed() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    console.log('rendering feed')
+    console.log("rendering feed");
     dispatch(fetchMessages());
   }, []);
   return (
@@ -43,9 +42,7 @@ export function MessageFeed() {
           likes={message.likes}
           key={message.id}
         />
-        
       ))}
-
     </>
   );
 }
