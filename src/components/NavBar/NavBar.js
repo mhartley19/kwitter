cca8b07fcf5ab5abded1f49d28d73e8783031198
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-09-11 14:39:06 -0400

    Added CSS features to messageItem, heart shape likes

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,33 +1,30 @@
-import React from 'react'
+import React from "react";
 
-import { useDispatch } from "react-redux"
+import { useDispatch } from "react-redux";
 import { actions } from "../../redux/actions/auth";
-import { Nav, Navbar, Form, } from "react-bootstrap"
+import { Nav, Navbar, Form } from "react-bootstrap";
 
 export const NavBar = () => {
-    const dispatch = useDispatch();
-    const logout = () => dispatch(actions.logout());
-    return(
-        
-        <Navbar bg="primary" variant="dark">
-      
-        <Nav className="mr-auto">
-          <Nav.Link href="/profiles/:username">Profile</Nav.Link>
-          <Nav.Link href="/messagefeed" >Message Feed</Nav.Link>
-          <Nav.Link onClick={logout}>Logout</Nav.Link>
-          <Navbar.Collapse className="justify-content-end">
-              
-              </Navbar.Collapse>
-          
-        </Nav>
-        
-        <Form inline>
-        
-        </Form>
-        
-      </Navbar>)
-      
-  
-    
-}
+  const dispatch = useDispatch();
+  const logout = () => dispatch(actions.logout());
+  return (
+    <Navbar
+      bg="primary"
+      variant="dark"
+      style={{
+        position: "sticky",
+        top: "0px",
+        zIndex: "1",
+      }}
+    >
+      <Nav className="mr-auto">
+        <Nav.Link href="/profiles/:username">Profile</Nav.Link>
+        <Nav.Link href="/messagefeed">Message Feed</Nav.Link>
+        <Nav.Link onClick={logout}>Logout</Nav.Link>
+        <Navbar.Collapse className="justify-content-end"></Navbar.Collapse>
+      </Nav>
 
+      <Form inline></Form>
+    </Navbar>
+  );
+};
