2fea870f3b482a8a62a734f45b6c5cc369f06380
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-09-12 15:48:59 -0400

    Added npm react-helmet. Changed tab name /

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { Helmet } from "react-helmet";
 import { PersistGate } from "redux-persist/integration/react";
 import { Navigation } from "./components";
 import configureStore from "./redux/configureStore";
@@ -17,6 +18,10 @@ window.KWITTER = KWITTER;
 export const App = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Kwitter | Query</title>
+      </Helmet>
       <Navigation />
     </PersistGate>
   </Provider>
