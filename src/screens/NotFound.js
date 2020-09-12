1193aa1b98521fd333344b981dd0dbefbacdb163
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-09-12 16:53:10 -0400

    Fixed 404 to appear in center of page

--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Screens.css'
-import './notFound.css'
+import "./notFound.css";
 
 const NotFound = ({ location }) => (
   <>
-  <div id="not-found"
-  >
-    <img id="image" 
-    src="https://www.butler.edu/sites/default/files/kenzie_logo.png" 
-    alt="logo"></img>
-    <div>
-    <p id="text">404 Not Found, Bruh</p>
-    <Link id="home-link" to="/">Go Home</Link>
+    <div id="not-found">
+      <img
+        id="image"
+        src="https://www.butler.edu/sites/default/files/kenzie_logo.png"
+        alt="logo"
+      ></img>
+      <div>
+        <p id="text">404 Not Found, Bruh</p>
+        <Link id="home-link" to="/">
+          Go Home
+        </Link>
+      </div>
     </div>
-    </div> 
   </>
 );
 
