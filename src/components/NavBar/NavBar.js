1b6be72bd59aec528e7dba04957b9efb1abc3b71
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-09-12 16:40:52 -0400

    Cleaned up code

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,34 +4,44 @@ import { useDispatch, useSelector } from "react-redux";
 import { actions } from "../../redux/actions/auth";
 import { Nav, Navbar, Form, Button } from "react-bootstrap";
 import { showModal, hideModal } from "../../redux/actions";
-import CreatePostModal from "../createPostModal/createPostModal"
-
-
+import CreatePostModal from "../createPostModal/createPostModal";
 
 export const NavBar = () => {
   const dispatch = useDispatch();
   const logout = () => dispatch(actions.logout());
   const show = useSelector((state) => state.postMessage.show);
 
-
   return (
     <Navbar
-
       variant="dark"
       style={{
         position: "sticky",
         top: "0px",
         height: "50px",
         zIndex: "2",
-        backgroundColor: "#343a40"
+        backgroundColor: "#343a40",
       }}
     >
       <Nav className="mr-auto">
-        <Button size="sm" variant="dark"><Nav.Link style={{ color: "white" }} href="/profiles/:username">Profile</Nav.Link></Button>
-        <Button size="sm" variant="dark"><Nav.Link style={{ color: "white" }} href="/messagefeed">Message Feed</Nav.Link></Button>
-        <Button size="sm" variant="dark"><Nav.Link style={{ color: "white" }} onClick={logout}>Logout</Nav.Link></Button>
+        <Button size="sm" variant="dark">
+          <Nav.Link style={{ color: "white" }} href="/profiles/:username">
+            Profile
+          </Nav.Link>
+        </Button>
+        <Button size="sm" variant="dark">
+          <Nav.Link style={{ color: "white" }} href="/messagefeed">
+            Message Feed
+          </Nav.Link>
+        </Button>
+        <Button size="sm" variant="dark">
+          <Nav.Link style={{ color: "white" }} onClick={logout}>
+            Logout
+          </Nav.Link>
+        </Button>
         <Navbar.Collapse className="justify-content-end"></Navbar.Collapse>
       </Nav>
+
+      <h1>Kwitter Query</h1>
 
       <Button variant="dark" onClick={() => dispatch(showModal())}>
         +
@@ -39,11 +49,7 @@ export const NavBar = () => {
 
       <Form inline></Form>
 
-      <CreatePostModal
-        show={show}
-        onHide={() => dispatch(hideModal())}
-      />
-
+      <CreatePostModal show={show} onHide={() => dispatch(hideModal())} />
     </Navbar>
   );
 };
--- src/components/NavBar/NavBarNewUser.js
+++ src/components/NavBar/NavBarNewUser.js
@@ -1,24 +1,16 @@
-import React from 'react'
-import { Nav, Navbar, Form, } from "react-bootstrap"
+import React from "react";
+import { Nav, Navbar, Form } from "react-bootstrap";
 
 export const NavBarNewUser = () => {
-  
-    return(
-        <>
-        <Navbar bg="primary" variant="dark">
-      
+  return (
+    <>
+      <Navbar bg="primary" variant="dark">
         <Nav className="mr-auto">
-        <Nav.Link href="/">Home</Nav.Link>
-            
-            
-            
-          
+          <Nav.Link href="/">Home</Nav.Link>
         </Nav>
-        
-        <Form inline>
-        
-        </Form>
-        
+
+        <Form inline></Form>
       </Navbar>
-      </>)
-}
+    </>
+  );
+};
