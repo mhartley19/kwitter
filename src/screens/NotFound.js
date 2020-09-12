ad829c5db8cd6b155431e61be4d4353db0baf46d
Mike Hartley <mhartley19@gmail.com>
2020-09-12 15:27:37 -0500

    styled 404 page

--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './screens.css'
+import './Screens.css'
+import './notFound.css'
 
 const NotFound = ({ location }) => (
   <>
-  <div id="not-found">
+  <div id="not-found"
+  >
     <img id="image" 
     src="https://www.butler.edu/sites/default/files/kenzie_logo.png" 
     alt="logo"></img>
     <div>
-    <p id="text">404, You have Failed</p>
+    <p id="text">404 Not Found, Bruh</p>
     <Link id="home-link" to="/">Go Home</Link>
     </div>
     </div> 
