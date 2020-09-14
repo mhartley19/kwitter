b9f366320eeafa43398a17c74ea39fde63fb320e
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-09-13 20:53:25 -0400

    Styled queued link at top of page

--- src/components/queued-posts/QueuedPosts.js
+++ src/components/queued-posts/QueuedPosts.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Button } from "react-bootstrap"
+import { Button } from "react-bootstrap";
 import {
   getRecents,
   mergeQueue,
@@ -24,8 +24,24 @@ function QueuedPosts({ lastId }) {
   }, [mergeQueue, recentMessage]);
 
   return (
-    <div style={{ margin: "auto", display: queue.length > 0 ? "block" : "none" }}>
-      <a style={{ color: "white" }} href="#top" onClick={() => dispatch(mergeQueue())}>
+    <div
+      style={{ margin: "auto", display: queue.length > 0 ? "block" : "none" }}
+    >
+      <a
+        className="queuedPost"
+        style={{
+          color: "white",
+          // backgroundColor: "#7d97f5",
+          fontSize: "20px",
+          fontWeight: "400",
+          padding: "5px 10px 9px 10px",
+          borderRadius: "4px",
+          border: "1px solid black",
+          boxShadow: "1px 4px 11px 1px black",
+        }}
+        href="#top"
+        onClick={() => dispatch(mergeQueue())}
+      >
         {queue.length} unread posts
       </a>
     </div>
